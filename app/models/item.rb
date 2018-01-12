80374192ccedea75fa29ba87c3e66f3891d33525
Lan Phan <lanc_phan@hotmail.com>
2018-01-11 21:31:49 -0500

    association and validation for item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  # associations
+  belongs_to :user
+
+  # validations
+  validates :title, presence: true, length: { maximum: 250 }, allow_blank: false
+  validates :id, uniqueness: true
 end
