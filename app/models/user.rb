9d5f19adcf8fb6ba984691e3fe2794c32c4e681f
Lan Phan <lanc_phan@hotmail.com>
2018-01-11 21:33:13 -0500

    add association and validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  # associations
+  has_many :items
+
+  # validations
+  validates :email, uniqueness: true
+  validates :password, length: {minimum: 8}, on: :create
+  validates :password, confirmation: true, on: :create
+  validates :password, presence: true, on: :create
 end
